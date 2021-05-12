103aeb0bace07c5877b1edcfaf694622d439d691
DuyenHo24 <duyen.24.07.dh@gmail.com>
2021-05-13 00:05:55 +0700

    navigation

---
+++ BTMoi/app/src/main/java/com/example/test_btmoi/activities/HomeActivity.java
@@ -0,0 +1,46 @@
+package com.example.test_btmoi.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.test_btmoi.MainActivity;
+import com.example.test_btmoi.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    //FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //auth = FirebaseAuth.getInstance();
+        //progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
+        //if (auth.getCurrentUser() != null) {
+            progressBar.setVisibility(View.VISIBLE);
+            startActivity(new Intent(HomeActivity.this, MainActivity.class));
+            Toast.makeText(this, "Please wait you are already logged in",
+                Toast.LENGTH_SHORT).show();
+            finish();
+        //}
+
+
+
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+    }
+
+    public void registration(View view) {
+        startActivity(new Intent(HomeActivity.this, RegistrationActivity.class));
+    }
+}
--- BTMoi/app/src/main/java/com/example/test_btmoi/activities/RegistrationActivity.java
+++ BTMoi/app/src/main/java/com/example/test_btmoi/activities/RegistrationActivity.java
@@ -1,8 +1,10 @@
-package com.example.test_btmoi;
+package com.example.test_btmoi.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.test_btmoi.R;
 
 public class RegistrationActivity extends AppCompatActivity {
 
